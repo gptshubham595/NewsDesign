23c6198c7adeaf0f0836f40bace475319f397e09
gptshubham595 <gptshubham595@gmail.com>
2018-12-16 13:46:03 +0530

    First

---
+++ app/src/main/java/ggps/shubham/newsdesign/MainActivity.java
@@ -0,0 +1,18 @@
+package ggps.shubham.newsdesign;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
