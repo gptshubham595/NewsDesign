474eb73f1298ac1116cb81960ca201390a976705
gptshubham595 <gptshubham595@gmail.com>
2018-12-17 20:31:08 +0530

    Next

--- app/src/main/java/ggps/shubham/newsdesign/MainActivity.java
+++ app/src/main/java/ggps/shubham/newsdesign/MainActivity.java
@@ -1,21 +1,29 @@
 package ggps.shubham.newsdesign;
 
+import android.view.GestureDetector;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 import org.objectweb.asm.Handle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
 RelativeLayout relative;
+    private GestureDetector gestureDetector;
+    public static  final int SWIPE_THRESHOLD=100;
+    public static  final int SWIPE_VELOCITY_THRESHOLD=100;
+
+ScrollView scrollView;
     ViewFlipper viewFlipper;
 
 int i=0;
@@ -51,15 +59,86 @@ int i=0;
                 },500);
             }
         });
+
     }
     public void  flippimage(int image){
         ImageView imageView=new ImageView(this);
         imageView.setBackgroundResource(image);
         viewFlipper.addView(imageView);
-        viewFlipper.setFlipInterval(4000);
+        viewFlipper.setFlipInterval(1800);
         viewFlipper.setAutoStart(true);
         viewFlipper.setInAnimation(this,android.R.anim.slide_in_left);
         viewFlipper.setOutAnimation(this,android.R.anim.slide_out_right);
+
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+
+    @Override
+    public boolean onFling(MotionEvent downevent, MotionEvent moveevent, float velocityX, float velocityY) {
+        boolean result=false;
+        float diffY=moveevent.getY() - downevent.getY();
+        float diffX=moveevent.getX() - downevent.getX();
+        if(Math.abs(diffX)>Math.abs(diffY)){
+            //right or left swipe
+            result=true;
+            if(Math.abs(diffX)>SWIPE_THRESHOLD && Math.abs(velocityX)>SWIPE_VELOCITY_THRESHOLD ){
+                if(diffX>0){onSwipeRight();}
+                else {onSwipeLeft();}
+
+            }
+
+        }
+        else{
+            //up or down swipe
+            result=true;
+            if(Math.abs(diffY)>SWIPE_THRESHOLD && Math.abs(velocityY)>SWIPE_VELOCITY_THRESHOLD){
+                if(diffY>0){onSwipeBottom();}
+                else{onSwipeTop();}
+            }
+        }
+
+        return result;
+    }
+
+    private void onSwipeRight() {
+        Toast.makeText(getApplicationContext(),"Right swipe",Toast.LENGTH_SHORT).show();
+        Intent a=new Intent(getApplicationContext(),Main3Activity.class);startActivity(a);
+    }
+    private void onSwipeLeft() {
+        Toast.makeText(getApplicationContext(),"Left swipe",Toast.LENGTH_SHORT).show();
+
+    }
+    private void onSwipeTop() {
+        Toast.makeText(getApplicationContext(),"Top swipe",Toast.LENGTH_SHORT).show();
+
+    }
+    private void onSwipeBottom() {
+        Toast.makeText(getApplicationContext(),"Down swipe",Toast.LENGTH_SHORT).show();
 
     }
 
